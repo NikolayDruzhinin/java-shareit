d5b2d36060a62a26d44dcf6788e401cc6db2f55a
Nikolay Druzhinin <northernNikolay@yandex.ru>
2025-07-21 00:12:47 +0300

    add-bookings feature

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -5,12 +5,29 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice("ru.practicum.shareit")
+@RestControllerAdvice
 public class ErrorHandler {
-
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse notFound(NotFoundException e) {
-        return new ErrorResponse(e.getMessage());
+    public ErrorResponse handleNotFound(NotFoundException e) {
+        return new ErrorResponse("Not Found", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidation(ValidationException e) {
+        return new ErrorResponse("Bad Request", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleValidation(ConflictException e) {
+        return new ErrorResponse("Conflict", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handlePermission(PermissionException e) {
+        return new ErrorResponse("Forbidden", e.getMessage());
     }
 }
--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,16 +1,14 @@
-package ru.practicum.shareit.user;
+package ru.practicum.shareit.user.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class UserDto {
-    @NotBlank
+    private Long id;
     private String name;
-    @NotBlank
     @Email
     private String email;
 }
