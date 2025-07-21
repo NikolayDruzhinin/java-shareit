2b0723943d1d0dca304e7a127361ed6c1c981f47
Nikolay Druzhinin <northernNikolay@yandex.ru>
2025-07-21 22:18:12 +0300

    add-bookings fix

--- src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -17,7 +17,7 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(name = "content")
     private String text;
     @ManyToOne
