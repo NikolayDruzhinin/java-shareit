61c7c174e757a80545af8711ca401e920aea7945
Nikolay Druzhinin <northernNikolay@yandex.ru>
2025-07-21 22:15:57 +0300

    add-bookings fix

--- src/main/java/ru/practicum/shareit/user/UserRepositoryExtensionImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserRepositoryExtensionImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import ru.practicum.shareit.exception.ConflictException;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.model.User;
 
@@ -17,21 +16,5 @@ public class UserRepositoryExtensionImpl implements UserRepositoryExtension {
                 .getResultStream()
                 .findFirst()
                 .orElseThrow(() -> new NotFoundException(String.format("User %d not found!", userId)));
-    }
-
-    @Override
-    public void throwIfEmailTaken(Long userId, String email) {
-        Long count = entityManager.createQuery("""
-                        SELECT COUNT(u)
-                          FROM User u
-                         WHERE u.email = :email
-                           AND u.id != :userId
-                        """, Long.class)
-                .setParameter("email", email)
-                .setParameter("userId", userId != null ? userId : 0)
-                .getSingleResult();
-        if (count > 0) {
-            throw new ConflictException(String.format("User with email %s already registered!", email));
-        }
     }
 }
